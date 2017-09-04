c7e314948b60815945bc6c5f5f856dd1855f03d8
tbhjelmeland <t.b.hjelmeland@fys.uio.no>
2017-09-04 16:50:40 +0200

    just an update

--- Simplecase/main.cpp
+++ Simplecase/main.cpp
@@ -1,6 +1,6 @@
-#include <iostream>
+#include <iostream>     //function for input and output to my function/main
 #include <fstream>
-#include <iomanip>
+#include <iomanip>      //manipulator function for in-/output streams
 #include <cmath>
 #include <string>
 // use namespace for output and input
@@ -9,25 +9,23 @@ using namespace std;
 // object for output files
 ofstream ofile;
 // Functions used
-double f(double x){
-    return 100.0*exp(-10.0*x);
-}
+double f(double x){return 100.0*exp(-10.0*x);}
 double exact(double x) {return 1.0-(1-exp(-10))*x-exp(-10*x);}
 
 // Begin main program
 int main(int argc, char *argv[]){
   int exponent;
     string filename;
-    // We read also the basic name for the output file and the highest power of 10^n we want
+    // We need two user input; first name for the output file and than n which defines the different matrix sizes
     if( argc <= 1 ){
           cout << "Bad Usage: " << argv[0] <<
-              " read also file name on same line and max power 10^n" << endl;
+              " We need two user input; first the basic name for the output file and than the highest power of 10^n we want on matrix size" << endl;
           exit(1);
     }
         else{
         filename = argv[1]; // first command line argument after name of program
         exponent = atoi(argv[2]);
-        cout << "Highest exponent in 10^n, define quadratic matrix sizes: " << argv[2] << endl;
+        cout << "Exponent in 10^n, define quadratic matrix sizes: " << argv[2] << endl;
         cout << "Output filename: " << argv[1] << endl;
     }
     // Loop over powers of 10
@@ -50,19 +48,35 @@ int main(int argc, char *argv[]){
      d[0] = d[n] = 2;
      //cout << "Before we added dialog elements, only endpoints: " << *d << endl;
      solution[0] = solution[n] = 0.0;
-     for (int i = 1; i < n; i++) d[i] = (i+1.0)/( (double) i);
+     //set value of x_0 to 0
+     //x[0]=0.0;
+     //set value of x_n to 1
+    // x[n]=1.0;
+     for (int i = 1; i < n; i++){
+         d[i] = (i+1.0)/( (double) i);
+         cout << "Diagonal element, tilde_d= " << d[i] << endl;
+     }
      for (int i = 0; i <= n; i++){
          x[i] = i*h;
          b[i] = hh*f(x[i]);
+         cout << "b= " << b[i] << endl;
+         cout << "x= " << *&x[i] << endl;
      }
      // Forward substitution
-     for (int i = 2; i < n; i++) b[i] = b[i] + b[i-1]/d[i-1];
+     for (int i = 2; i < n; i++){
+         b[i] = b[i] + b[i-1]/d[i-1];
+         cout << "b_tilde = " << b[i] << endl;
+     }
      // Backward substitution
      solution[n-1] = b[n-1]/d[n-1];
-     for (int i = n-2; i > 0; i--) solution[i] = (b[i]+solution[i+1])/d[i];
+     cout << "solution_n-1 = " << solution[n-1] << endl;
+     for (int i = n-2; i > 0; i--){
+         solution[i] = (b[i]+solution[i+1])/d[i];
+         cout << "Solution = " << solution[i] << endl;
+     }
      ofile.open(fileout);
      ofile << setiosflags(ios::showpoint | ios::uppercase);
-     //      ofile << "       x:             approx:          exact:       relative error" << endl;
+     ofile << "       x:             approx:          exact:       relative error" << endl;
      for (int i = 1; i < n;i++) {
       double RelativeError = fabs((exact(x[i])-solution[i])/exact(x[i]));
         ofile << setw(15) << setprecision(8) << x[i];
